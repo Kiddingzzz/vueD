76f04dbcf112fdd9e337b1c88b0d4cc5fe15184f
XSLu08042 <1831783941@qq.com>
2019-11-25 15:00:49 +0800

    fix:tuijian

--- src/mytables/tuijian.vue
+++ src/mytables/tuijian.vue
@@ -32,24 +32,73 @@
 
                </div>
            </a-tab-pane>
-           <a-tab-pane tab="历史推荐" key="2"></a-tab-pane>
+           <a-tab-pane tab="历史推荐" key="2">
+               <a-table :columns="columns" :dataSource="data">
+                   <!-- <span slot="ation" slot-scope="text, record">
+                        <a href="javascript:;" @click="onDelete(record.key)">删除</a>
+                        <a href="javascript:;" @click="onfabu(record.key)" >未发布</a>
+                         </span> -->
+                </a-table>
+           </a-tab-pane>
         </a-tabs>
   </div>
 </template>
  <script>
     const tiles=['发布','采集'];
+    const columns = [
+        {
+            title: '网站名称',
+            dataIndex: 'name',
+        },
+        {
+            title: '网站地址',
+            dataIndex: 'url',
+        },
+        {
+            title: '网站账号',
+            dataIndex: 'wzzhanghao',
+        },
+        {
+            title: '推荐时间',
+            dataIndex: 'time',
+        },
+        {
+            title: '审核状态',
+            dataIndex: 'auditstatus',
+        },
+        {
+            title: '操作',
+            dataIndex: 'active',
+            scopedSlots: { customRender: 'action' },
+        },
+    ];
+
+   const data = [];
+    for (let i = 0; i < 6; i++) {
+        data.push({
+            key: i,
+            name:'花卉园',
+            url: 'https://cq.fang.com',
+            wzzhanghao: '08042',
+            time: '20191116',
+            auditstatus: '审核中',
+        });
+    }
     export default {
         name:'tuijian',
         data() {
             return {
                 value1:'发布',
                 tiles,
+                columns,
+                data
             };
             
             },
        
        
-        };
+    };
+    
  </script>
  <style>
        .ant-layout, .ant-layout *{
