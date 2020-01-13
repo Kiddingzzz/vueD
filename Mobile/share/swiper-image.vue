5f6ac3ebef386c8b731821fbef5e4a95ec64cbc8
Dengz <377978704@qq.com>
2020-01-13 17:52:24 +0800

    ------------------------------------------------------------------------------
    Beyond Compare 4
    by Scooter Software                                    www.scootersoftware.com
    ------------------------------------------------------------------------------
    1. 描述
    -------
    Beyond Compare是一个Windows平台上的文件和文件夹比较工具。使用该工具可以可视化和调整差异，
    合并修改，同步文件夹。
    2. 系统要求
    -----------
    该程序运行于：
    - Windows XP
    - Windows Server 2003，Windows Server 2003 x64
    - Windows Vista，Windows Vista x64
    - Windows Server 2008，Windows Server 2008 x64
    - Windows 7，Windows 7 x64
    - Windows Server 2008 R2
    - Windows 8，Windows 8 x64
    - Windows Server 2012
    - Windows Server 2012 R2
    - Windows 10，Windows 10 x64
    我们的网站上也有Mac和Linux版本的程序。
    3. 评估模式
    -----------
    你可以免费的评估Beyond Compare 30天（实际使用时间）。30天之后该程序需要一个许可证密钥才能继续工作。
    有关购买或升级许可证的信息，请访问：
        http://www.scootersoftware.com/buynow
    4. 联系人信息
    -------------
    如果你有问题，意见或建议，我们希望收到你的信息。如果要报告bug，你可以点击“帮助”菜单内的“支持”来获得程序设置的转储。
    复制并粘贴设置信息到你的email中帮助我们追查到问题。
        Email:  support@scootersoftware.com 或 sales@scootersoftware.com
        网站:   http://www.scootersoftware.com/

---
+++ Mobile/share/swiper-image.vue
@@ -0,0 +1,114 @@
+<template>
+	<view class="topsd">
+		<view class="uni-padding-wrap">
+			<view class="page-section swiper">
+				<view class="page-section-spacing">
+					<swiper class="swiper1" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+						<!-- <swiper-item class="swiper-shifts" v-for="(item,index) in bannerlist.items" :key="index" :value="item">
+							<image :src="item.imageKey" class="swiper-item uni-bg-red1" ></image>
+						</swiper-item> -->
+						<swiper-item>
+							<view class="swiper-item uni-bg-red">A</view>
+						</swiper-item>
+						<swiper-item>
+							<view class="swiper-item uni-bg-green">B</view>
+						</swiper-item>
+						<swiper-item>
+							<view class="swiper-item uni-bg-blue">C</view>
+						</swiper-item>
+					</swiper>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				title: 'swiper',
+				background: ['color1', 'color2', 'color3'],
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500,
+				bannerlist: {},
+				linkurl:'',
+			}
+		},
+		mounted() {
+			//this.getBannerList();
+		},
+		methods: {
+			async getBannerList() {
+				var data = {
+					providerName: '资讯'
+				};
+
+				var res = await this.$http.get(
+					`api/eShop/banner/mobileBannerList`, data
+				);
+				Object.assign(this.$data, res);
+				this.bannerlist = res;
+				console.log(JSON.stringify(this.bannerlist));
+			},
+			changeIndicatorDots(e) {
+				this.indicatorDots = !this.indicatorDots
+			},
+			changeAutoplay(e) {
+				this.autoplay = !this.autoplay
+			},
+			intervalChange(e) {
+				this.interval = e.target.value
+			},
+			durationChange(e) {
+				this.duration = e.target.value
+			},
+		}
+	}
+</script>
+
+<style>
+	.topsd{
+		width: 100%;
+		height: 100%;
+	}
+	.swiper1 {
+		height: 300upx;
+	}
+
+	.swiper-item {
+		display: block;
+		height: 300upx;
+		line-height: 300upx;
+		text-align: center;
+		justify-content: center;
+		align-items: center;
+	}
+	.uni-bg-red1{
+		width: 710upx;
+		line-height: 300upx;
+	}
+	.swiper-shifts{
+		border-radius: 10upx;
+	}
+	.swiper-list {
+		margin-top: 40upx;
+		margin-bottom: 0;
+	}
+
+	.uni-common-mt {
+		margin-top: 60upx;
+		position: relative;
+	}
+
+	.page-section-spacing {
+		flex-flow: column;
+		justify-content: center;
+	}
+
+	.info {
+		position: absolute;
+		right: 20upx;
+	}
+</style>
