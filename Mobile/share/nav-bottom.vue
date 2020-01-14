f34ebe27f1e4addd6d8ea041194ad178ce654a23
XSLu08042 <1831783941@qq.com>
2020-01-14 09:30:54 +0800

    fix:底部

--- Mobile/share/nav-bottom.vue
+++ Mobile/share/nav-bottom.vue
@@ -1,49 +1,84 @@
 <template>
 	<view class="layout">
-		<view class="lianxiren">
-			<image src="../static/HM-PersonalCenter/face.jpg"></image>
-			<view class="info">
-				
+		<view class="tab-bottom">
+			<view class="lianxiren">
+				<image class="imageinfo" src="https://pic1.ajkimg.com/display/anjuke/237c94b741db9d0b3508e652ba32a4f4/200x265x0x45/100x133.jpg"></image>
+				<view class="info">
+					张媛媛
+				</view>
 			</view>
-		</view>
-		<view class="tab-bottom" v-for="(item,index) in options" :key="index">
-			<view class="item">
-				<image class="image" :src="item.icon"/>
-				<view class="text">{{item.text}}</view>
+			<view class="item1">
+				<image class="image" src="https://img-cdn-qiniu.dcloud.net.cn/uniapp/uni-ui/goodsnav/dianpu.png" />
+				<view class="text">电话</view>
+			</view>
+			<view class="item2">
+				<image class="image" src="https://img-cdn-qiniu.dcloud.net.cn/uniapp/uni-ui/goodsnav/carts.png" />
+				<view class="text">收藏</view>
 			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				options:[{
-						icon: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/uni-ui/goodsnav/dianpu.png',
-						text: '店铺'
-					}, {
-						icon: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/uni-ui/goodsnav/carts.png',
-						text: '购物车'
-					}]
-			};
-		}
+export default {
+	data() {
+		return {
+			
+		};
 	}
+};
 </script>
 
 <style lang="less">
-.layout{
+.layout {
 	width: 100%;
 }
-.tab-bottom{
-	display:flex;
-	.item{
-		display:flex;
-		flex-direction: column;
+.tab-bottom {
+	display: flex;
+	align-items: center;
+	height: 100upx;
+	width: 100%;
+	position: fixed;
+	bottom: 0upx;
+	right: 0upx;
+	background: #fff;
+	z-index: 999;
+	box-shadow: 0px -1px 6px 0 rgba(0, 0, 0, 0.14);
+	color: #FFFFFF;
+	.lianxiren{
+		height: 100%;
+		width: 33.33%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		color: #000;
+	}
+	.item1{
+		height: 100%;
+		width: 33.33%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-image: linear-gradient(90deg, #3CDC50 0%, #3CB950 100%);
+	}
+	.item2{
+		height: 100%;
+		width: 33.33%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-image: linear-gradient(90deg, #FF9800 0%, #FF7500 100%);
 	}
 }
 .image {
-		width: 20px;
-		height: 20px;
-	}
+	width: 40upx;
+	height: 40upx;
+	margin-right: 20upx;
+}
+.imageinfo{
+	width: 70upx;
+	height: 70upx;
+	border-radius: 50%;
+	margin-right: 20upx;
+}
 </style>
