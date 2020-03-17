640070f3c77762310325ecdbd159f423aa1f182a
XSLu08042 <1831783941@qq.com>
2020-03-17 11:30:26 +0800

    fix:video页面修改

--- src/views/videos/video.vue
+++ src/views/videos/video.vue
@@ -3,41 +3,63 @@
         <div>
             <label class='headsize'>观看下列视频，可使您快速掌握开单王网站</label>
         </div>
-        <div v-show="ishows"> 
-            <video controls="" src="//xms-course-video.obs.cn-east-2.myhwclouds.com/4846/190220-register.mp4" id="ckplayer_a1" width="1000" height="600" autoplay="autoplay" loop="loop"></video>
-            </div>
+        
         <div  v-show="isshow">
-             <div class="videheard">
-                <div class="videbox"  >
-                    <img src="http://www.4846.com/Public/images/icon-video.png" alt="">
-                    <div class="sizes"><label>01-如何注册开单王</label></div>
+             <div class="videheard" v-for="(item,i) in video" :key="i">
+                <div class="videbox" @click="change(i)">
+                    <img :src="item.picname" alt="">
+                    <div class="sizes"><label>{{item.title}}</label></div>
                 </div>
-                <div class="videbox" @click="change">
-                    <img src="http://www.4846.com/Public/images/icon-video.png" alt="">
-                    <div class="sizes"><label>01-如何注册开单王</label></div>
-                </div>
-                <div class="videbox">
-                    <img src="http://www.4846.com/Public/images/icon-video.png" alt="">
-                    <div class="sizes"><label>01-如何注册开单王</label></div>
-                </div>
-             </div>
-            
+             </div>           
+        </div>
+        <div class="bobox">
+            <div v-show="ishows"> 
+                <video controls="" :src="videosrc" id="ckplayer_a1" width="1000" height="600" autoplay="autoplay" loop="loop"></video>   
+            </div>
+            <div v-show="boshow" class="bolist">
+                <a-list itemLayout="horizontal" :dataSource="video">
+                    <a-list-item slot="renderItem" slot-scope="item, index">
+                    <a-list-item-meta>
+                        <a slot="title" @click="change(index)" :class="{ 'blue-link':index === isActive }" >{{item.title}}</a>
+                    </a-list-item-meta>
+                    </a-list-item>
+                </a-list>
+            </div>
         </div>
     </div>
 </template>
-<script>
+<script> 
+  const video =[
+    {
+      title: '01-如何注册开单王',
+      picname: 'http://www.4846.com/Public/images/icon-video.png',
+      src: '//xms-course-video.obs.cn-east-2.myhwclouds.com/4846/190220-register.mp4',
+    },
+    {
+      title: '02-如何注册开单王',
+      picname: 'http://www.4846.com/Public/images/icon-video.png',
+      src: '//xms-course-video.obs.cn-east-2.myhwclouds.com/4846/6-1.mp4',
+    },
+  ];
 export default {
      data() {
             return {
-                isshow:true,
-                ishows:false
+                video,
+                videosrc:"",
+                isshow: true,
+                ishows: false,
+                boshow: false,
+                isActive: 0,
             }},
     methods:{
-        change(){
-           this.ishows=true
-           this.isshow=false
-          
-        }
+        change(i){
+            console.log('页面点击：'+this.video[i].src)
+            this.videosrc = this.video[i].src
+            this.ishows = true
+            this.isshow = false  
+            this.boshow = true
+            this.isActive = i   
+        },
     }
 }
 </script>
@@ -56,4 +78,22 @@ export default {
       display: flex;
       justify-content: center;  
 }
+.blue-link{
+    color: #007acc !important;
+}
+.bobox{
+    margin: 0 auto;
+    display: flex;
+    .bolist{
+        padding: 0 20px;
+        background: #1e1e1e;
+        a{
+            color: #eee;
+            &:hover{
+                color: #007acc;
+            }
+        }
+    }
+}
+
 </style>
